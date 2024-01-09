9badf3b92e3f3d792ae54e508ab3b75ae42a0ce0
joacojachow <joacojachow@hotmail.com>
2024-01-09 14:39:24 -0300

    now all setted

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <meta name='author' content='Joaquin Jachow' />
+      </Head>
       <body>
         <Main />
         <NextScript />
