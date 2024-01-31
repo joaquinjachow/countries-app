ad1bc6ec6af28d8ce015bc228423c998f74c965e
joacojachow <joacojachow@hotmail.com>
2024-01-31 03:34:10 -0300

    routes init

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const CountrySchema = require('../models/CountryModel')
+const ActivitySchema = require('../models/ActivityModel')
 const router = express.Router()
 
-// create user
-router.post('/users', (req, res) => {
-  const user = CountrySchema(req.body)
-  user
+router.get('/countries', (req, res) => {
+  const country = CountrySchema(req.body)
+  country
     .save()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }))
 })
-
+router.post('/activity', (req, res) => {
+  const activity = ActivitySchema(req.body)
+  activity
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }))
+})
 module.exports = router
