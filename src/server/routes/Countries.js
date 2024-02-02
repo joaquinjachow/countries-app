85901518ffb562c4f19367a7e88ec7dd80ab9a6a
joacojachow <joacojachow@hotmail.com>
2024-02-02 02:09:40 -0300

    Routes change

---
+++ src/server/routes/Countries.js
@@ -0,0 +1,9 @@
+// routes/countries.js
+const { Router } = require('express')
+const { getCountries } = require('../controllers/countryController')
+
+const router = Router()
+
+router.get('/', getCountries)
+
+module.exports = router
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,20 +1,8 @@
-const express = require('express')
-const CountrySchema = require('../models/CountryModel')
-const ActivitySchema = require('../models/ActivityModel')
-const router = express.Router()
+const { Router } = require('express')
+const countryRouter = require('./countries')
 
-router.get('/countries', (req, res) => {
-  const country = CountrySchema(req.body)
-  country
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }))
-})
-router.post('/activity', (req, res) => {
-  const activity = ActivitySchema(req.body)
-  activity
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }))
-})
+const router = Router()
+
+router.use('/countries', countryRouter)
+
 module.exports = router
