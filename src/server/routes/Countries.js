a62bfcba4ddff94462ac23e28aed195c309fc02a
joacojachow <joacojachow@hotmail.com>
2024-02-11 14:55:20 -0300

    Countries routes change

--- src/server/routes/Countries.js
+++ src/server/routes/Countries.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
-const { getCountries } = require('../controllers/countryController')
+const { getCountries, getCountryById } = require('../controllers/countryController')
 
 const router = Router()
 
 router.get('/', getCountries)
+router.get('/:id', getCountryById)
 
 module.exports = router
