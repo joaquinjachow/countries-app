08f1b98a4e4c649c66788c193163844455faf42e
joacojachow <joacojachow@hotmail.com>
2024-02-19 02:28:14 -0300

    button change

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,15 +9,38 @@ export default function LandingPage () {
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center'
   }
-  console.log(background)
+
   return (
     <div className='flex justify-center min-h-screen items-center' style={containerStyle}>
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center space-y-5'>
         <h1 className='text-6xl text-[#3A301A]'>Paises del mundo</h1>
         <Link href='/home'>
-          <button className='px-4 py-2 font-extrabold text-xl cursor-pointer border-none rounded-3xl overflow-hidden bg-[#A98069] text-[#503A31]'>Ingresar</button>
+          <button className='px-4 py-2 font-extrabold text-xl cursor-pointer border-none rounded-3xl overflow-hidden bg-[#A98069] text-[#503A31] landingBtn'>Ingresar</button>
         </Link>
       </div>
+      <style jsx>{`
+        .landingBtn {
+          position: relative;
+        }
+        .landingBtn::before {
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: 0;
+          background: #2f211b;
+          width: 120%;
+          left: -10%;
+          transform: skew(30deg);
+          transition: transform 0.4s cubic-bezier(0.3, 1, 0.8, 1);
+        }
+        .landingBtn:hover::before {
+          transform: translate3d(100%, 0, 0);
+        }
+      `}
+      </style>
     </div>
   )
 }
