0912911317842cdfe0d93c84c0fbe6e61914c476
joacojachow <joacojachow@hotmail.com>
2024-03-16 01:35:50 -0300

    max high in the card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ const Card = ({ name, flag, continent, id }) => {
           </Link>
         </div>
         <div>
-          <Image width={100} height={1} className='bg-transparent w-[90%] mx-auto' src={flag} alt='No se encontro la bandera' />
+          <Image width={100} height={1} className='bg-transparent w-[90%] mx-auto max-h-36' src={flag} alt='No se encontro la bandera' />
         </div>
         <div>
           <h3>{continent}</h3>
