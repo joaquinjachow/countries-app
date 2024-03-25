23521439b1ddc626bfbb3bde4a3e921caf5a2ed4
joacojachow <joacojachow@hotmail.com>
2024-03-25 15:40:31 -0300

    coments deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,19 +17,8 @@ const Home = () => {
   const [input, setInput] = useState(1)
   const CurrentCountries = countries === 'No se encontro el pais' ? '0' : countries
   const max = Math.ceil(CurrentCountries?.length ? CurrentCountries.length / countriesPag : CurrentCountries.length / countriesPag)
-  /*   const [order, setOrder] = useState('')
-  const [orderP, setOrderPopulation] = useState('') */
 
-  /*   function handleClick (e) {
-    e.preventDefault()
-    setOrder('')
-    setOrderPopulation('')
-    document.getElementById('borrar').getElementsByTagName('option')[0].selected = 'selected'
-    document.getElementById('borrar1').getElementsByTagName('option')[0].selected = 'selected'
-    document.getElementById('borrar2').getElementsByTagName('option')[0].selected = 'selected'
-    document.getElementById('borrar3').getElementsByTagName('option')[0].selected = 'selected'
-  }
-  function handleSortName (e) {
+  /*   function handleSortName (e) {
     e.preventDefault()
     setCurrentPage(1)// seteo para que la pagina default arranque en 1
     setOrder(`Sort ${e.target.value}`) // para que cuando setea la pagina mododifique el estado local y se renderize
@@ -51,35 +40,6 @@ const Home = () => {
   return (
     <div className='mx-auto'>
       <Navbar />
-      {/*         <select onChange={e => handleSortName(e)} defaultValue='default' id='borrar'>
-          <option value='default' disabled>Ordenado Alfabetico</option>
-          <option value='asc'> A-Z</option>
-          <option value='des'> Z-A</option>
-        </select>
-        <select defaultValue='default' onChange={e => handleSortPopulation(e)} id='borrar1'>
-          <option value='default' disabled>Ordenado por poblacion</option>
-          <option value='des'> Mayor Poblacion</option>
-          <option value='asc'> Menor Poblacion</option>
-        </select>
-        <select onChange={e => handleFilterByActivities(e)} id='borrar2'>
-          <option value='all'>Actividades</option>
-          <option value='act'>Con Actividades</option>
-          <option value='noA'>Sin Actividades</option>
-        </select>
-        <select onChange={e => handleFilterContinents(e)} id='borrar3'>
-          <option value='All'>Continent</option>
-          <option value='Africa'>Africa</option>
-          <option value='Antarctica'>Antarctica</option>
-          <option value='Asia'>Asia</option>
-          <option value='Europe'>Europe</option>
-          <option value='North America'>North America</option>
-          <option value='South America'>South America</option>
-          <option value='Oceania'>Oceania</option>
-        </select>
-        <button onClick={e => { handleFilterPopulation2(e) }}>
-          Paises con poblacion mayor a 10.000.000
-        </button>
-        <button onClick={e => { handleClick(e) }}> Limpiar filtros</button> */}
       <Paginado
         pag={pag}
         setPag={setPag}
