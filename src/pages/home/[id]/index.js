afe89395585de5fa17ff1e41302deabd4a7ef3d7
joacojachow <joacojachow@hotmail.com>
2024-03-31 20:31:47 -0300

    Details page added

---
+++ src/pages/home/[id]/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Detail from '../../../components/Detail'
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Paises | Countries-app</title>
+      </Head>
+      <main>
+        <Detail />
+      </main>
+    </>
+  )
+}
+export default Index
