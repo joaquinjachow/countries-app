d7e8705722e9ae3511bb365541007dff14fbd002
joacojachow <joacojachow@hotmail.com>
2024-06-19 18:13:45 -0300

    Searchbar change

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react'
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
   const [name, setName] = useState('')
 
-  function handleInputChange (e) {
-    e.preventDefault()
+  const handleInputChange = e => {
     setName(e.target.value)
   }
-  function handleSubmit (e) {
+
+  const handleSubmit = e => {
     e.preventDefault()
+    onSearch(name.trim())
     setName('')
   }
 
   return (
     <div className='flex items-center h-[4vh]'>
       <input
-        className='cursor-auto border-none rounded-3xl bg-white h-full w-[55vh] text-black text-lg text-left pl-4'
+        className='cursor-auto border-none rounded-3xl px-4 bg-white h-full w-[55vh] text-black text-lg text-left pl-4'
         value={name}
         onChange={e => handleInputChange(e)}
         type='search'
         placeholder='Buscar Pais'
       />
-      <button className='cursor-pointer border-none bg-none h-full w-20 text-lg m-1 items-center text-[#f0ffff] rounded-lg' type='submit' onClick={e => handleSubmit(e)}>Buscar</button>
+      <button
+        className='cursor-pointer border-none bg-none h-full w-20 text-lg m-1 items-center text-[#f0ffff] rounded-lg'
+        type='submit'
+        onClick={handleSubmit}
+      >
+        Buscar
+      </button>
     </div>
   )
 }
