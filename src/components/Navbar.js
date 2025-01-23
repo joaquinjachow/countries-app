651cd0ee9a0ba65797d4d37916d43d8b7a13e600
joacojachow <joacojachow@hotmail.com>
2025-01-23 19:27:47 -0300

    navbar improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,25 @@ import Image from 'next/image'
 
 const Navbar = ({ onSearch }) => {
   return (
-    <nav className='flex flex-nowrap justify-between h-32 w-auto content-start items-center mb-3' style={{ backgroundImage: "url('/assets/Banner.jpeg')", backgroundSize: 'cover' }}>
+    <nav
+      className='flex flex-nowrap justify-between h-32 items-center mb-3 px-8'
+      style={{
+        backgroundColor: '#A98069'
+      }}
+    >
       <div>
         <Link href='/'>
-          <Image className='mx-12 p-8' src={Logo} width={160} alt='logo' />
+          <Image className='p-4' src={Logo} width={140} alt='logo' />
         </Link>
       </div>
       <div>
         <Searchbar onSearch={onSearch} />
       </div>
       <div>
-        <Link href='/activity' className='no-underline'>
-          <button className='cursor-pointer font-semibold text-base border-none bg-none h-6 w-auto text-white m-1 justify-between flex flex-row-reverse items-center content-start pr-16 tracking-wide border-b border-transparent transition-border duration-200'>NUEVA ACTIVIDAD</button>
+        <Link href='/createActivity'>
+          <button className='cursor-pointer text-base rounded-lg bg-white text-[#A98069] font-medium shadow-md hover:bg-[#8C6A54] hover:text-white px-6 py-2 transition duration-300'>
+            NUEVA ACTIVIDAD
+          </button>
         </Link>
       </div>
     </nav>
