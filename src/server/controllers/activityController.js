6a30f8840677204d210ffd26e70fc09fed99f80f
joacojachow <joacojachow@hotmail.com>
2025-03-04 17:28:08 -0300

    controller improved

--- src/server/controllers/activityController.js
+++ src/server/controllers/activityController.js
@@ -3,26 +3,31 @@ const { Country, Activity } = require('../db')
 const postActivity = async (req, res, next) => {
   const { name, dificulty, duration, season, countriesName } = req.body
   try {
-    if (name && dificulty && duration && season && countriesName) {
-      const activity = {
-        name,
-        dificulty,
-        season,
-        duration
+    if (name && dificulty && duration && season && countriesName.length > 0) {
+      const countries = await Country.find({ name: { $in: countriesName } })
+      if (countries.length === 0) {
+        return res.status(404).json({ message: 'No se encontraron los países' })
       }
-      const createdActivity = await Activity.create(activity)
-      const infoCountriesName = await Country.find({
-        name: {
-          $in: countriesName
-        }
-      })
-
-      infoCountriesName.forEach((country) => {
-        country.activities.push(createdActivity._id)
-        country.save()
-      })
-      if (createdActivity) { res.json({ message: 'Se creo correctamente la Actividad', data: createdActivity }) } else res.json({ message: 'Error, no se obtuvieron suficientes datos' })
+      let activity = await Activity.findOne({ name, dificulty, duration, season })
+      if (!activity) {
+        activity = await Activity.create({
+          name,
+          dificulty,
+          duration,
+          season,
+          countries: countries.map(c => c._id)
+        })
+      } else {
+        activity.countries.push(...countries.map(c => c._id))
+        await activity.save()
+      }
+      await Country.updateMany(
+        { _id: { $in: countries.map(c => c._id) } },
+        { $push: { activities: activity._id } }
+      )
+      return res.json({ message: 'Actividad creada con éxito', data: activity })
     }
+    res.status(400).json({ message: 'Datos insuficientes' })
   } catch (error) {
     next(error)
   }
