39c5c6f3923759152c6e0131f4acc873b93f8a99
joacojachow <joacojachow@hotmail.com>
2025-03-16 19:02:59 -0300

    navbar improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,17 @@ import Image from 'next/image'
 const Navbar = ({ onSearch }) => {
   return (
     <nav
-      className='flex flex-nowrap justify-between h-32 items-center mb-3 px-8'
+      className='flex flex-nowrap justify-between items-center h-32 shadow-md mb-3 px-8'
       style={{
         backgroundColor: '#A98069'
       }}
     >
       <div>
         <Link href='/'>
-          <Image className='p-4' src={Logo} width={140} alt='logo' />
+          <Image className='p-4 cursor-pointer' src={Logo} width={140} alt='logo' />
         </Link>
       </div>
-      <div>
-        <Searchbar onSearch={onSearch} />
-      </div>
+      <Searchbar onSearch={onSearch} />
       <div>
         <Link href='/createActivity'>
           <button className='cursor-pointer text-base rounded-lg bg-white text-[#A98069] font-medium shadow-md hover:bg-[#8C6A54] hover:text-white px-6 py-2 transition duration-300'>
