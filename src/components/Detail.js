9424507c2b06e10c3ea1bebf8d87f0b3323616c9
joacojachow <joacojachow@hotmail.com>
2025-03-24 19:32:47 -0300

    detail improved and toast added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import useStore from '../store/zustandStore'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Detail = () => {
-  const { getCountries, updateCountryActivities, countries } = useStore()
+  const router = useRouter()
+  const { id } = router.query
+  const { getCountries, countries, addActivityToCountry } = useStore()
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [errors, setErrors] = useState({})
   const [input, setInput] = useState({
@@ -15,8 +20,16 @@ const Detail = () => {
   })
 
   useEffect(() => {
-    getCountries()
-  }, [])
+    if (countries.length === 0) {
+      getCountries()
+    }
+  }, [countries.length, getCountries])
+
+  const selectedCountry = countries.find((country) => country.id === id)
+
+  if (!selectedCountry) {
+    return <p className='text-3xl font-bold text-center'>Cargando...</p>
+  }
 
   const validate = (input) => {
     const errors = {}
@@ -30,92 +43,88 @@ const Detail = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target
-    setInput({ ...input, [name]: value })
-    setErrors(validate({ ...input, [name]: value }))
+    setInput((prevInput) => {
+      const updatedInput = { ...prevInput, [name]: value }
+      setErrors(validate(updatedInput))
+      return updatedInput
+    })
   }
 
-  const handleSaveActivity = () => {
+  const handleSaveActivity = async () => {
     const validationErrors = validate(input)
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors)
       return
     }
-    if (countries.length === 0) {
-      alert('No hay países cargados.')
+    if (!selectedCountry) {
+      toast.error('No se encontró el país.')
       return
     }
-    const newActivity = { ...input }
-    const countryId = countries[0].id
-    updateCountryActivities(countryId, newActivity)
+    await addActivityToCountry(selectedCountry.id, input)
     setInput({ name: '', dificulty: '', duration: '', season: '' })
     setIsModalOpen(false)
+    toast.success('Actividad agregada con éxito!')
   }
 
   return (
     <div className='container mx-auto pt-20'>
-      {countries.length > 0
-        ? (
-          <div className='flex flex-col items-center'>
-            <div className='bg-white p-16 rounded-lg shadow-lg shadow-slate-500 border border-gray-300 flex flex-col md:flex-row items-center max-w-4xl'>
-              <div className='flex-shrink-0 mb-6 md:mb-0 md:mr-8'>
-                <Image
-                  width={300}
-                  height={200}
-                  className='w-full h-auto object-cover rounded-lg'
-                  src={countries[0].flag}
-                  alt={`Bandera de ${countries[0].name}`}
-                />
-              </div>
-              <div className='text-lg flex-grow'>
-                <h1 className='text-4xl font-bold mb-4 text-center md:text-left'>{countries[0].name}</h1>
-                <div className='space-y-2'>
-                  <p>
-                    <strong>ID:</strong> {countries[0].id}
-                  </p>
-                  <p>
-                    <strong>Capital:</strong> {countries[0].capital}
-                  </p>
-                  <p>
-                    <strong>Continente:</strong> {countries[0].subregion}
-                  </p>
-                  <p>
-                    <strong>Área:</strong> {countries[0].area} km²
-                  </p>
-                  <p>
-                    <strong>Población:</strong> {countries[0].population}
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            {countries[0].activities.length > 0 && (
-              <div className='mt-8 bg-[#8fa4cf81] p-6 rounded-lg shadow-md border border-black max-w-4xl w-full'>
-                <h2 className='text-2xl font-bold mb-4'>Actividades:</h2>
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-                  {countries[0].activities.map((e) => (
-                    <div key={e.id} className='text-lg'>
-                      <p>
-                        <strong>Nombre:</strong> {e.name}
-                      </p>
-                      <p>
-                        <strong>Dificultad:</strong> {e.dificulty}
-                      </p>
-                      <p>
-                        <strong>Duración:</strong> {e.duration}
-                      </p>
-                      <p>
-                        <strong>Temporada:</strong> {e.season}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
+      <div className='flex flex-col items-center'>
+        <div className='bg-white p-16 rounded-lg shadow-lg shadow-slate-500 border border-gray-300 flex flex-col md:flex-row items-center max-w-4xl'>
+          <div className='flex-shrink-0 mb-6 md:mb-0 md:mr-8'>
+            <Image
+              width={300}
+              height={200}
+              className='object-cover rounded-lg'
+              src={selectedCountry.flag}
+              alt={`Bandera de ${selectedCountry.name}`}
+            />
           </div>
-          )
-        : (
-          <p className='text-3xl font-bold text-center'>Cargando...</p>
-          )}
+          <div className='text-lg flex-grow'>
+            <h1 className='text-4xl font-bold mb-4 text-center md:text-left'>{selectedCountry.name}</h1>
+            <div className='space-y-2'>
+              <p>
+                <strong>ID:</strong> {selectedCountry.id}
+              </p>
+              <p>
+                <strong>Capital:</strong> {selectedCountry.capital}
+              </p>
+              <p>
+                <strong>Continente:</strong> {selectedCountry.subregion}
+              </p>
+              <p>
+                <strong>Área:</strong> {selectedCountry.area} km²
+              </p>
+              <p>
+                <strong>Población:</strong> {selectedCountry.population}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {selectedCountry.activities.length > 0 && (
+          <div className='mt-8 bg-[#8fa4cf81] p-6 rounded-lg shadow-md border border-black max-w-4xl w-full'>
+            <h2 className='text-2xl font-bold mb-4'>Actividades:</h2>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+              {selectedCountry.activities.map((e) => (
+                <div key={e.id} className='text-lg'>
+                  <p>
+                    <strong>Nombre:</strong> {e.name}
+                  </p>
+                  <p>
+                    <strong>Dificultad:</strong> {e.dificulty}
+                  </p>
+                  <p>
+                    <strong>Duración:</strong> {e.duration}
+                  </p>
+                  <p>
+                    <strong>Temporada:</strong> {e.season}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
 
       <div className='flex justify-center mt-8'>
         <button
@@ -133,8 +142,8 @@ const Detail = () => {
       </Link>
 
       {isModalOpen && (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-          <div className='bg-white p-8 rounded-lg shadow-lg max-w-lg w-full'>
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md transition-opacity duration-300'>
+          <div className='bg-white p-8 rounded-lg shadow-xl max-w-lg w-full transform transition-all scale-95'>
             <h2 className='text-2xl font-bold mb-4'>Crear Actividad</h2>
             <div className='space-y-4'>
               <div>
