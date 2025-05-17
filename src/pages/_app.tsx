64366467a32a8608fb198e99bbeb9a7bb5f3a76c
joacojachow <joacojachow@hotmail.com>
2025-05-17 16:18:52 -0300

    toast added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { ToastContainer } from 'react-toastify'
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -12,6 +13,7 @@ const roboto = Roboto({
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <div className={roboto.className}>
+      <ToastContainer />
       <Component {...pageProps} />
     </div>
   )
