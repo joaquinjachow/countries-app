de35247abe9216de53e7693cae6a99697f892d9c
joacojachow <joacojachow@hotmail.com>
2025-12-27 02:20:43 -0300

    screens added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,18 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ]
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: '475px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      }
+    }
+  }
 }
 export default config
