5ddd7c5893ffcbfb77585ab65d9445cd81d1326c
joacojachow <joacojachow@hotmail.com>
2025-12-31 17:12:30 -0300

    navbar improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,22 @@ import Image from 'next/image'
 const Navbar = ({ onSearch }) => {
   return (
     <nav
-      className='flex flex-nowrap justify-between items-center h-32 shadow-md mb-3 px-8'
+      className='flex flex-col sm:flex-row flex-nowrap justify-between items-center min-h-32 sm:h-32 shadow-md mb-3 px-4 sm:px-8 py-4 sm:py-0 gap-4 sm:gap-0'
       style={{
         backgroundColor: '#A98069'
       }}
     >
-      <div>
+      <div className='order-1 sm:order-1'>
         <Link href='/'>
-          <Image className='p-4 cursor-pointer' src={Logo} width={140} alt='logo' />
+          <Image className='p-2 sm:p-4 cursor-pointer' src={Logo} width={120} height={60} alt='logo' />
         </Link>
       </div>
-      <Searchbar onSearch={onSearch} />
-      <div>
+      <div className='order-2 sm:order-2 w-full sm:w-auto'>
+        <Searchbar onSearch={onSearch} />
+      </div>
+      <div className='order-3 sm:order-3'>
         <Link href='/createActivity'>
-          <button className='cursor-pointer text-base rounded-lg bg-white text-[#A98069] font-medium shadow-md hover:bg-[#8C6A54] hover:text-white px-6 py-2 transition duration-300'>
+          <button className='cursor-pointer text-sm sm:text-base rounded-lg bg-white text-[#A98069] font-medium shadow-md hover:bg-[#8C6A54] hover:text-white px-4 sm:px-6 py-2 transition duration-300 whitespace-nowrap'>
             NUEVA ACTIVIDAD
           </button>
         </Link>
